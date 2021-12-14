8347087954c183747245714ef049f85aa24e0e17
brysonbest <brycekalden@gmail.com>
2021-12-13 22:15:11 -0800

    Add tweets section and formatting.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 //Script that updates the character counter for new tweets
 $(document).ready(function() {
-  let tweetText = document.getElementById("tweet-text");
+  const tweetText = document.getElementById("tweet-text");
   tweetText.addEventListener("input", function() {
-    let text = String($(this).val());
-    let count = 140 - (text.length);
-    let counter = this.parentElement.querySelector('.counter');
+    const text = String($(this).val());
+    const count = 140 - (text.length);
+    const counter = this.parentElement.querySelector('.counter');
     $(counter).text(count);
     if (count < 0) {
       counter.setAttribute('id', 'counterNegative');
